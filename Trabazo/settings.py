b5dea7b20b07cf0ae5971876ef6e4ffd60577c75
deepak895 <dac81048@gmail.com>
2017-04-04 16:22:01 +0530

    database fixes added

--- Trabazo/settings.py
+++ Trabazo/settings.py
@@ -87,11 +87,14 @@ EMAIL_PORT = 587
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trabazo',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
