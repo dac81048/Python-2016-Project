8e05dc16016377c3a65481c787fbda79b8798b77
yogi-katewa <yogikatewa@gmail.com>
2017-04-10 18:36:51 +0530

    remove dATABASE engine

--- Trabazo/settings.py
+++ Trabazo/settings.py
@@ -90,7 +90,7 @@ EMAIL_PORT = 587
 
 DATABASES = {
     'default': 'postgres://fvpzurbmjjecjh:f3ffb2ab3b3f96b05250ede96041076f75743c312ee4b3dc7c8190b50fec817b@ec2-54-197-232-155.compute-1.amazonaws.com:5432/df0ttivrnlrhvh'}
-DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+    #DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
