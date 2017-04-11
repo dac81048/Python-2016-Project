b32afe456d21711ec21cd64b9f260173bb5535a5
yogi-katewa <yogikatewa@gmail.com>
2017-04-11 10:56:48 +0530

    database setting

--- Trabazo/settings.py
+++ Trabazo/settings.py
@@ -90,15 +90,10 @@ EMAIL_PORT = 587
 import dj_database_url
 DATABASES = {
     'default':'postgres://fvpzurbmjjecjh:f3ffb2ab3b3f96b05250ede96041076f75743c312ee4b3dc7c8190b50fec817b@ec2-54-197-232-155.compute-1.amazonaws.com:5432/df0ttivrnlrhvh'
-        
-    
-}
- # DATABASES = {
- #     'default': 'postgres://fvpzurbmjjecjh:f3ffb2ab3b3f96b05250ede96041076f75743c312ee4b3dc7c8190b50fec817b@ec2-54-197-232-155.compute-1.amazonaws.com:5432/df0ttivrnlrhvh'}
+        }
      DATABASES['default'] = dj_database_url.config()
-
+     DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 #DATABASES['default'] =  dj_database_url.config()
-#DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
